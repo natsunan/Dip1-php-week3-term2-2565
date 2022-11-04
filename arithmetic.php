f3998817e849c620af147fd677b9464ad8b87bd1
Natsunan <Natsunan270@gmail.com>
2022-11-04 16:49:36 +0700

    up

---
+++ arithmetic.php
@@ -0,0 +1,22 @@
+<?php
+//Arithemiti Operator
+$x= 10;
+$y = 4;
+
+#1.Addition
+echo "$x+$y = ",$x + $y,"<br>";
+
+#2. Subtraction
+echo "$x-$y = ",$x - $y,"<br>";
+
+#3.Multiply
+echo "$x*$y =",$x * $y,"<br>";
+
+#4. Division
+echo "$x/ $y =",$x / $y,"<br>";
+
+#5.Modulus Remaining
+echo "$x% $y =",$x % $y,"<br>";
+
+#6 . Exponential
+echo "$x**$y =",$x ** $y,"<br>";
---
+++ assignment.php
@@ -0,0 +1,22 @@
+<?php
+$x = 10
+
+#1.Add and Assignment value
+echo  "$x +=20=",$x+=20,"<br>"; //x = x +20//30
+
+#2. Subtract Assignmnt value
+echo  "$x -=20=",$x-=20,"<br>"; //x = x -20//10
+
+#3. multiply Assignmnt value
+echo  "$x *=20=",$x*=20,"<br>"; //x = x *20//
+
+#4. Division Assignmnt value
+echo  "$x /=20=",$x/=20,"<br>"; //x = x /20//
+
+#5. Modulus Assignmnt value
+echo  "$x %=20=",$x%=20,"<br>"; //x = x %20//
+
+#6. Exponential  Assignmnt value
+echo  "$x **=20=",$x**=20,"<br>"; //x = x **20//
+
+?>
